62380ee69c868f1c4cd14ab5472cfb73565ec7f8
Leo Ngari <ngareoel@gmail.com>
2022-05-11 01:35:40 +0300

    Fixed relative path issue

--- src/helpers.py
+++ src/helpers.py
@@ -1,4 +1,6 @@
 from pandas import read_csv
+from os import getcwd
+from pathlib import Path
 
 def time_parser(time: str) -> int:
     """Converts the time col for comparison purpose"""
@@ -6,10 +8,11 @@ def time_parser(time: str) -> int:
     return int(hours) * 100 + int(minutes)
 
 def open_csv_file():
-    file_location: str = "./res/train_revised.csv"
+    file_location: str = "res/train_revised.csv"
     chunk_size: int = 1000
     convertors: list = {
         "travel_time": time_parser
     }   
-    return read_csv(file_location, chunksize=chunk_size, header=0, index_col=0, converters=convertors)
+    actual_location = Path(__file__).parent.parent / file_location
+    return read_csv(actual_location, chunksize=chunk_size, header=0, index_col=0, converters=convertors)
 
